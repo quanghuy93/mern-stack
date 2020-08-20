fb1e3b8ce0f01b5dd834e19571faf702595dc144
HuyPink <quanghuynt93@gmail.com>
2020-08-20 09:48:09 +0700

    Session 3: 13-14 Session 4: 15-19

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,10 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const {
-  check,
-  validationResult
-} = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -67,9 +64,8 @@ router.post(
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        {
-          expiresIn: 360000
-        }, (err, token) => {
+        { expiresIn: 360000 },
+        (err, token) => {
         if (err) throw err;
         res.json({token});
       })
