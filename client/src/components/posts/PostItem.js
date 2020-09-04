b7230dbb6b5176bb302c5205e070a5e023a194b5
HuyPink <quanghuynt93@gmail.com>
2020-09-04 18:37:39 +0700

    Section 65 - 66

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -7,26 +7,26 @@ import {addLike, removeLike, deletePost} from '../../actions/post';
 
 const PostItem = ({addLike, removeLike, deletePost, auth, post: {_id, text, name, avatar, user, likes, comments, date}}) => {
   return (
-    <div class="post bg-white p-1 my-1">
+    <div className="post bg-white p-1 my-1">
       <div>
-        <a href="profile.html">
-          <img class="round-img" src={avatar} alt="" />
+        <Link to={`/profile/${user}`}>
+          <img className="round-img" src={avatar} alt="" />
           <h4>{name}</h4>
-        </a>
+        </Link>
       </div>
       <div>
-        <p class="my-1">{text}</p>
-        <p class="post-date">Posted on <Moment format="YYYY/MM/DD">{date}</Moment></p>
-        <button type="button" class="btn btn-light" onClick={e => addLike(_id)}>
-          <i class="fas fa-thumbs-up"></i>
+        <p className="my-1">{text}</p>
+        <p className="post-date">Posted on <Moment format="YYYY/MM/DD">{date}</Moment></p>
+        <button type="button" className="btn btn-light" onClick={e => addLike(_id)}>
+          <i className="fas fa-thumbs-up"></i>
           <span>{likes.length > 0 && (<span> {likes.length}</span>)}</span>
         </button>
-        <button type="button" class="btn btn-light" onClick={e => removeLike(_id)}><i class="fas fa-thumbs-down"></i></button>
-        <Link to={`/post/${_id}`} class="btn btn-primary">
-          Discussion {comments.length > 0 && (<span class='comment-count'>{comments.length}</span>)}
+        <button type="button" className="btn btn-light" onClick={e => removeLike(_id)}><i className="fas fa-thumbs-down"></i></button>
+        <Link to={`/post/${_id}`} className="btn btn-primary">
+          Discussion {comments.length > 0 && (<span className='comment-count'>{comments.length}</span>)}
         </Link>
         {!auth.loading && user === auth.user._id && (
-          <button type="button" class="btn btn-danger" onClick={e => deletePost(_id)}><i class="fas fa-times"></i></button>
+          <button type="button" className="btn btn-danger" onClick={e => deletePost(_id)}><i className="fas fa-times"></i></button>
         )}
       </div>
     </div>
